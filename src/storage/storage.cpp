a7d56251fb5ad560dfc63856bc65f970ff266af0
Huyen Chau Nguyen <hello@chau-nguyen.de>
2016-10-20 16:23:56 -0700

    fix wrong block size allocation in shared memory

--- src/storage/storage.cpp
+++ src/storage/storage.cpp
@@ -343,12 +343,16 @@ Storage::ReturnCode Storage::Run(int max_wait)
     }
     std::uint64_t number_of_compressed_datasources = 0;
     if (geometry_datasource_input_stream)
-        number_of_compressed_datasources = io::readDatasourceIndexesSize(geometry_datasource_input_stream);
+    {
+        number_of_compressed_datasources =
+            io::readDatasourceIndexesSize(geometry_datasource_input_stream);
+    }
     shared_layout_ptr->SetBlockSize<uint8_t>(SharedDataLayout::DATASOURCES_LIST,
                                              number_of_compressed_datasources);
 
     // Load datasource name sizes.  This file is optional, and it's non-fatal if it doesn't
     // exist
+
     boost::filesystem::ifstream datasource_names_input_stream(config.datasource_names_path,
                                                               std::ios::binary);
     if (!datasource_names_input_stream)
@@ -358,7 +362,9 @@ Storage::ReturnCode Storage::Run(int max_wait)
     }
     io::DatasourceNamesData datasource_names_data;
     if (datasource_names_input_stream)
+    {
         datasource_names_data = io::readDatasourceNamesData(datasource_names_input_stream);
+    }
 
     shared_layout_ptr->SetBlockSize<char>(SharedDataLayout::DATASOURCE_NAME_DATA,
                                           datasource_names_data.names.size());
@@ -369,6 +375,7 @@ Storage::ReturnCode Storage::Run(int max_wait)
 
     boost::filesystem::ifstream intersection_stream(config.intersection_class_path,
                                                     std::ios::binary);
+
     if (!static_cast<bool>(intersection_stream))
         throw util::exception("Could not open " + config.intersection_class_path.string() +
                               " for reading.");
@@ -632,6 +639,7 @@ Storage::ReturnCode Storage::Run(int max_wait)
     }
 
     // load datasource name information (if it exists)
+
     char *datasource_name_data_ptr = shared_layout_ptr->GetBlockPtr<char, true>(
         shared_memory_ptr, SharedDataLayout::DATASOURCE_NAME_DATA);
     if (shared_layout_ptr->GetBlockSize(SharedDataLayout::DATASOURCE_NAME_DATA) > 0)
@@ -641,7 +649,7 @@ Storage::ReturnCode Storage::Run(int max_wait)
                   datasource_name_data_ptr);
     }
 
-    auto datasource_name_offsets_ptr = shared_layout_ptr->GetBlockPtr<std::size_t, true>(
+    auto datasource_name_offsets_ptr = shared_layout_ptr->GetBlockPtr<std::uint32_t, true>(
         shared_memory_ptr, SharedDataLayout::DATASOURCE_NAME_OFFSETS);
     if (shared_layout_ptr->GetBlockSize(SharedDataLayout::DATASOURCE_NAME_OFFSETS) > 0)
     {
@@ -650,7 +658,7 @@ Storage::ReturnCode Storage::Run(int max_wait)
                   datasource_name_offsets_ptr);
     }
 
-    auto datasource_name_lengths_ptr = shared_layout_ptr->GetBlockPtr<std::size_t, true>(
+    auto datasource_name_lengths_ptr = shared_layout_ptr->GetBlockPtr<std::uint32_t, true>(
         shared_memory_ptr, SharedDataLayout::DATASOURCE_NAME_LENGTHS);
     if (shared_layout_ptr->GetBlockSize(SharedDataLayout::DATASOURCE_NAME_LENGTHS) > 0)
     {
